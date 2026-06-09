d5d458bf4c5b31e868a7818e20bee8a1fe409ee9
Naveryeon <21adtydwf@gmail.com>
2026-06-09 11:11:49 +0700

    add: update main screen to use BerandaMahasiswaScreen and clean up theme configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:suara_mawa/screens/aspirasi/beranda_mahasiswa/beranda_mahasiswa_screen.dart';
 import 'package:suara_mawa/screens/penindak/penindak_main_screen.dart';
 import 'package:suara_mawa/utils/app_colors.dart';
 
@@ -15,10 +16,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SUARA MAWA',
-      theme: ThemeData(
-        fontFamily: 'PublicSans'
-      ),
-      home: PenindakMainScreen()
+      theme: ThemeData(fontFamily: 'PublicSans'),
+      // home: PenindakMainScreen(),
+      home: const BerandaMahasiswaScreen(),
     );
   }
 }
